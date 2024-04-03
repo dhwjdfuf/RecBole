19418da6c1146bb6ec0c818068bc08b794c29dac
dhwjdfuf <63962278+dhwjdfuf@users.noreply.github.com>
2024-04-03 17:01:41 +0900

    Update ryeol.py

--- ryeol.py
+++ ryeol.py
@@ -1,9 +1,3 @@
 from recbole.quick_start import run_recbole
-import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--version', required=True)
-
-args = parser.parse_args()
-
-run_recbole(model='SASRec', dataset='shorts', config_file_list=['our_default.yaml'], version = args.version)
+run_recbole(model='SASRec', dataset='shorts', config_file_list=['our_default.yaml'])
